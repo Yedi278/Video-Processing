9daadc3aecd5818ea48eb7525c00fb999784833d
Yehan Edirisinghe <yedi278@gmail.com>
2023-12-21 13:12:03 +0100

    multiprocessing done

--- video.py
+++ video.py
@@ -1,11 +1,14 @@
 import cv2 as cv
 import numpy as np
-from threading import Thread
-import concurrent.futures as con
+from numpy.ctypeslib import as_ctypes
+
+import ctypes as c
+from multiprocessing import Process,RawArray
+
 import time
 
-path_input = '/home/peppo/Documents/Video_processing/test.mp4'
-path_output = '/home/peppo/Documents/Video_processing/output.mp4'
+path_input  = r"/home/peppo/Documents/Video_processing/test.mp4"
+path_output = r"/home/peppo/Documents/Video_processing/output.avi"
 
 def render(cap,out):
 
@@ -29,44 +32,36 @@ def render(cap,out):
         if not ret:             # if frame is read correctly ret is True
             print("Can't receive frame (stream end?). Exiting ...")
             break
-
-        tmp = np.empty(frame.shape,dtype=np.uint8)
-
-        # k = 2
-        # threads = []
-
-        # for i in range(k):
-
-        #     rng = range(int((i)*frame_height/k),int((i+1)*frame_height/k))
-
-        #     # x = Process(target=func, args=(frame,oldFrame,tmp,rng,range(int(frame_width))),daemon=True)
-            
-        #     threads.append(x)
-
-        # for i in range(k):
-        #     threads[i].start()
-        #     threads[i].join()
         
-        # with con.ThreadPoolExecutor() as executor:
-        #     futures = []
+        tmp = np.zeros(frame.shape,dtype=np.uint8)
 
-        #     k = 2
-        #     for i in range(k):
+        t = RawArray(c.c_uint8, (frame.shape[0]*frame.shape[1]*frame.shape[2]))
+        
+        t_np = np.frombuffer(t,dtype=np.uint8).reshape(frame.shape)
+        
+        np.copyto(t_np,tmp)
 
-        #         rng = range(int((i)*frame_height/k),int((i+1)*frame_height/k))
+        processes = list()
+        n=2
+        k = int(frame_height/n)
 
-        #         x = executor.submit(target=func, args=(frame,oldFrame,tmp,rng,range(int(frame_width))),daemon=True)
-            
-        #         futures.append(x)
+        for i in range(n):
 
-        func(frame,oldFrame,tmp,range(frame_height),range(frame_width))
+            p = Process(target=func, args=(frame,oldFrame,t_np, range(i*k,(i+1)*k),range(frame_width)))
+            processes.append(p)
+            p.start()
 
-        out.write(tmp)
+        for i in range(n):
+            processes[i].join()
+
+        grey = cv.cvtColor(t_np, cv.COLOR_BGR2GRAY)
+        out.write(grey)
 
         oldFrame[:] = frame
 
+        # cv.imshow('frame', cv.cvtColor(frame, cv.COLOR_BGR2GRAY))
+        # cv.imshow('frame', grey)
         
-        cv.imshow('frame', cv.cvtColor(frame, cv.COLOR_BGR2GRAY))
         counter += 1
 
     cap.release()
@@ -77,13 +72,10 @@ def func(frame,oldFrame,tmp,xlim,ylim):
 
     for x in xlim:
             for y in ylim:
-
                 for i in range(3):
-                    
-                    # tmp[x,y,i] = abs(oldFrame[x,y,i]-frame[x,y,i])
-                    tmp[x,y,i] = 0
+                    tmp[x,y,i] = abs(oldFrame[x,y,i]-frame[x,y,i])
 
-    # return tmp
+
 
 if __name__ == '__main__':
 
@@ -92,7 +84,7 @@ if __name__ == '__main__':
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     
-    out = cv.VideoWriter(path_output,cv.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+    out = cv.VideoWriter(path_output,cv.VideoWriter_fourcc(*'MJPG'), 10, (frame_width,frame_height))
 
 
     a = time.time()
