6618ffea7ffed5e6d45c21f6261a6d6b92f11d99
Yehan Edirisinghe <yedi278@gmail.com>
2023-12-21 13:29:08 +0100

    .

--- video.py
+++ video.py
@@ -10,12 +10,15 @@ import time
 path_input  = r"/home/peppo/Documents/Video_processing/test.mp4"
 path_output = r"/home/peppo/Documents/Video_processing/output.avi"
 
-def render(cap,out):
+def render(cap,out,n_processes=2):
 
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
 
     oldFrame = np.zeros((frame_height,frame_width,3))
+    
+    t = RawArray(c.c_uint8, (frame_height*frame_width*3))
+    t_np = np.frombuffer(t,dtype=np.uint8).reshape((frame_height,frame_width,3))
 
     counter = 0
 
@@ -29,37 +32,34 @@ def render(cap,out):
 
         ret, frame = cap.read() 
 
+        gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+
         if not ret:             # if frame is read correctly ret is True
             print("Can't receive frame (stream end?). Exiting ...")
             break
         
         tmp = np.zeros(frame.shape,dtype=np.uint8)
 
-        t = RawArray(c.c_uint8, (frame.shape[0]*frame.shape[1]*frame.shape[2]))
-        
-        t_np = np.frombuffer(t,dtype=np.uint8).reshape(frame.shape)
-        
         np.copyto(t_np,tmp)
 
         processes = list()
-        n=2
-        k = int(frame_height/n)
 
-        for i in range(n):
+        k = int(frame_height/n_processes)
+
+        for i in range(n_processes):
 
             p = Process(target=func, args=(frame,oldFrame,t_np, range(i*k,(i+1)*k),range(frame_width)))
             processes.append(p)
             p.start()
 
-        for i in range(n):
+        for i in range(n_processes):
             processes[i].join()
 
-        grey = cv.cvtColor(t_np, cv.COLOR_BGR2GRAY)
-        out.write(grey)
+        out.write(t_np)
 
         oldFrame[:] = frame
 
-        # cv.imshow('frame', cv.cvtColor(frame, cv.COLOR_BGR2GRAY))
+        # grey = cv.cvtColor(t_np, cv.COLOR_BGR2GRAY)
         # cv.imshow('frame', grey)
         
         counter += 1
@@ -70,10 +70,16 @@ def render(cap,out):
 
 def func(frame,oldFrame,tmp,xlim,ylim):
 
+    print(frame)
     for x in xlim:
             for y in ylim:
-                for i in range(3):
-                    tmp[x,y,i] = abs(oldFrame[x,y,i]-frame[x,y,i])
+
+                if abs(oldFrame[x,y]-frame[x,y]) > 3:
+                    tmp[x,y] = 255
+
+                # for i in range(3):
+                    
+                #     tmp[x,y,i] = abs(oldFrame[x,y,i]-frame[x,y,i])
 
 
 
@@ -86,8 +92,11 @@ if __name__ == '__main__':
     
     out = cv.VideoWriter(path_output,cv.VideoWriter_fourcc(*'MJPG'), 10, (frame_width,frame_height))
 
+    n = 2
 
     a = time.time()
-    render(cap,out)
+    render(cap,out,n)
     b = time.time()
-    print(b-a)
+
+    print(f"Number of processes:\t{n}")
+    print("execution Time:\t",b-a)
